a9a5e1ee8b7b135e6175a6bdf7d20eb01131cd69
vincent VB <42698353+vincentbudianto@users.noreply.github.com>
2019-03-12 23:39:46 +0700

    Add files via upload

---
+++ Graph.cs
@@ -0,0 +1,112 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tubes2Stima
+{
+    public class Graph
+    {
+        // Atributes
+        private int houses;
+        private List<int>[] paths;
+
+        // Constructor
+        public Graph(string filename)
+        {
+            System.IO.StreamReader fileRead = new System.IO.StreamReader(filename);
+
+            string fileLine;
+            fileLine = fileRead.ReadLine();
+            houses = Int32.Parse(fileLine);
+
+            // Initialize list
+            paths = new List<int>[houses + 1];
+            for (int i = 1; i <= houses; i++)
+            {
+                paths[i] = new List<int>();
+            }
+
+            // Add to list
+            while ((fileLine = fileRead.ReadLine()) != null)
+            {
+                string[] info = fileLine.Split();
+                paths[Int32.Parse(info[0])].Add(Int32.Parse(info[1]));
+                paths[Int32.Parse(info[1])].Add(Int32.Parse(info[0]));
+            }
+
+            // Sort and remove duplicates
+            for (int i = 1; i <= houses; i++)
+            {
+                paths[i] = paths[i].Distinct().ToList();
+                paths[i].Sort();
+            }
+
+            // 1-way Graph
+            delReverse(1);
+        }
+
+        // Destructor
+        ~Graph()
+        {
+            for (int i = 1; i <= houses; i++)
+            {
+                paths[i] = null;
+                GC.Collect();
+            }
+            paths = null;
+            GC.Collect();
+        }
+
+        // Getter-setter
+        public List<int> getPath(int node)
+        {
+            return (paths[node]);
+        }
+        public int getHouses()
+        {
+            return (houses);
+        }
+
+        // Other functions
+        private void delReverse(int node)
+        {
+            for (int i = 0; i < paths[node].Count; i++) {
+                paths[(paths[node][i])].Remove(node);
+                delReverse(paths[node][i]);
+            }
+        }
+        
+        public void print()
+        {
+            Console.WriteLine("Houses = {0}", houses);
+            Console.WriteLine("Palace is connected to: ");
+            for (int i = 1; i <= houses; i++)
+            {
+                Console.Write(i + ": ");
+                for (int j = 0; j < paths[i].Count; j++)
+                {
+                    Console.Write(paths[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+//             foreach (List<Int32> a in paths)
+//             {
+//                 //List<int> b = new List<int> (a);
+// //                Console.WriteLine(paths[1][1]);
+//                // Console.WriteLine("Count : " + a.Count);
+// //                 List<int> b = new List<int>(a);
+// //                 Console.WriteLine("TesterA");
+// //                 b.ForEach(Console.Write);
+// //                 Console.WriteLine();
+// //                 foreach (Int32 value in a)
+// //                 {
+// //                     Console.WriteLine("TesterB");
+// //                     Console.WriteLine(value);
+// //                     Console.WriteLine("TesterC");
+// //                 }
+//             } 
+        }
+    }
+}
---
+++ Query.cs
@@ -0,0 +1,111 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tubes2Stima
+{
+    public class Query
+    {
+        // Atributes
+        private int move;
+        private int from;
+        private int to;
+
+        // Constructor
+        public Query(string input)
+        {
+            string[] info = input.Split();
+            move = Int32.Parse(info[0]);
+            from = Int32.Parse(info[1]);
+            to = Int32.Parse(info[2]);
+        }
+
+        // getter-setter
+        public int getMove()
+        {
+            return move;
+        }
+        public int getFrom()
+        {
+            return from;
+        }
+        public int getTo()
+        {
+            return to;
+        }
+    }
+
+    // Class dari sebuah query
+    // Query dimuat dalam class ini dari index 0
+    public class ExQuery
+    {
+        // Atributes
+        private int num_queries;
+        private int[] move;
+        private int[] from;
+        private int[] to;
+
+        // Constructor
+        public ExQuery (string filename)
+        {
+            System.IO.StreamReader fileRead = new System.IO.StreamReader(filename);
+
+            string fileLine;
+            fileLine = fileRead.ReadLine();
+            num_queries = Int32.Parse(fileLine);
+
+            // Initialize array
+            move = new int[num_queries];
+            from = new int[num_queries];
+            to = new int[num_queries];
+
+            // Add to array
+            for (int i = 0; i < num_queries; i++)
+            {
+                fileLine = fileRead.ReadLine();
+                string[] info = fileLine.Split();
+                move[i] = Int32.Parse(info[0]);
+                to[i] = Int32.Parse(info[1]);
+                from[i] = Int32.Parse(info[2]);
+            }
+        }
+
+        // Destruktor
+        ~ExQuery()
+        {
+            move = null;
+            from = null;
+            to = null;
+            GC.Collect();
+        }
+
+        // getter-setter
+        public int getNum ()
+        {
+            return num_queries;
+        }
+        public int getMove(int i)
+        {
+            return move[i];
+        }
+        public int getFrom(int i)
+        {
+            return from[i];
+        }
+        public int getTo(int i)
+        {
+            return to[i];
+        }
+
+        // fungsi lain
+        public void print() {
+            Console.WriteLine("Printing Queries...");
+            for (int i = 0; i < num_queries; i++)
+            {
+                Console.WriteLine("Move = {0}, From = {1}, To = {2}", move[i], from[i], to[i]);
+            }
+        }
+    }
+}
