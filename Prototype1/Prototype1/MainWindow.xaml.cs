e85e8f0bad73d758693f9727ba0628501e395592
vincent VB <vincent.budianto@yahoo.co.id>
2019-03-16 21:53:44 +0700

    Fixed Final

--- Prototype1/Prototype1/MainWindow.xaml.cs
+++ Prototype1/Prototype1/MainWindow.xaml.cs
@@ -23,21 +23,49 @@ namespace Prototype1
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    
+    public class AutoClosingMessageBox
+    {
+        System.Threading.Timer _timeoutTimer;
+        string _caption;
+        AutoClosingMessageBox(string text, string caption, int timeout)
+        {
+            _caption = caption;
+            _timeoutTimer = new System.Threading.Timer(OnTimerElapsed,
+                null, timeout, System.Threading.Timeout.Infinite);
+            using (_timeoutTimer)
+                MessageBox.Show(text, caption);
+        }
+        public static void Show(string text, string caption, int timeout)
+        {
+            new AutoClosingMessageBox(text, caption, timeout);
+        }
+        void OnTimerElapsed(object state)
+        {
+            IntPtr mbWnd = FindWindow("#32770", _caption); // lpClassName is #32770 for MessageBox
+            if (mbWnd != IntPtr.Zero)
+                SendMessage(mbWnd, WM_CLOSE, IntPtr.Zero, IntPtr.Zero);
+            _timeoutTimer.Dispose();
+        }
+        const int WM_CLOSE = 0x0010;
+        [System.Runtime.InteropServices.DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        [System.Runtime.InteropServices.DllImport("user32.dll", CharSet = System.Runtime.InteropServices.CharSet.Auto)]
+        static extern IntPtr SendMessage(IntPtr hWnd, UInt32 Msg, IntPtr wParam, IntPtr lParam);
+    }
+
     public partial class MainWindow : Window
     {
+        public Boolean B1, B2;
         private DispatcherTimer _timer, _timer2;
         private ExQuery quests;
         private Graf map;
-        private int counter, checker;
+        private int checker, counter, now, prev;
         private List<int> solution;
-        private Query quest;
-        public Boolean B1, B2;
         public Msagl.Graph graph = new Msagl.Graph("graph");
+        private Query quest;
         public String dirGraph, dirExQuery;
-
-        private int prev;
-        private int now;
-
+        
         public MainWindow()
         {
             B1 = false;
@@ -45,11 +73,11 @@ namespace Prototype1
             InitializeComponent();
 
             _timer = new DispatcherTimer();
-            _timer.Interval = TimeSpan.FromSeconds(0.5);
+            _timer.Interval = TimeSpan.FromMilliseconds(0.000000001);
             _timer.Tick += timer_Tick_Green;
 
             _timer2 = new DispatcherTimer();
-            _timer2.Interval = TimeSpan.FromSeconds(0.5);
+            _timer2.Interval = TimeSpan.FromMilliseconds(0.000000001);
             _timer2.Tick += timer_Tick_Red;
         }
 
@@ -59,6 +87,7 @@ namespace Prototype1
             {
                 map = new Graf(dirGraph);
                 Enter_Query.IsEnabled = true;
+                Open_Query.IsEnabled = true;
                 Next.IsEnabled = false;
                 this.gViewer.Graph = null;
                 graph = new Msagl.Graph("graph");
@@ -147,6 +176,7 @@ namespace Prototype1
 
                 if (!res.Contains(quests.getTo(counter)))
                 {
+                    AutoClosingMessageBox.Show("Solution Not Found", "Result", 1500);
                     ans = "   TIDAK\n";
                     Result.Content += ans;
                     resetGraph();
@@ -154,10 +184,9 @@ namespace Prototype1
                 }
                 else
                 {
+                    AutoClosingMessageBox.Show("Solution Found", "Result", 1500);
                     ans = "   YA\n";
                     Result.Content += ans;
-                    //checker = 0;
-                    //_timer.Start();
                 }
             }
             else if (quests.getMove(counter) == 1)
@@ -173,14 +202,13 @@ namespace Prototype1
 
                 if (found3)
                 {
+                    AutoClosingMessageBox.Show("Solution Found", "Result", 1500);
                     ans = "   YA\n";
                     Result.Content += ans;
-
-                    //checker = 0;
-                    //_timer.Start();
                 }
                 else
                 {
+                    AutoClosingMessageBox.Show("Solution Not Found", "Result", 1500);
                     ans = "   TIDAK\n";
                     Result.Content += ans;
                     resetGraph();
@@ -223,6 +251,7 @@ namespace Prototype1
 
                     if (!res.Contains(quest.getTo()))
                     {
+                        AutoClosingMessageBox.Show("Solution Not Found", "Result", 1500);
                         ans = "   TIDAK\n";
                         Result.Content += ans;
                         resetGraph();
@@ -230,11 +259,9 @@ namespace Prototype1
                     }
                     else
                     {
+                        AutoClosingMessageBox.Show("Solution Found", "Result", 1500);
                         ans = "   YA\n";
                         Result.Content += ans;
-
-                        //checker = 0;
-                        //_timer.Start();
                     }
                 }
                 else if (quest.getMove() == 1)
@@ -249,15 +276,13 @@ namespace Prototype1
 
                     if (found3)
                     {
+                        AutoClosingMessageBox.Show("Solution Found", "Result", 1500);
                         ans = "   YA\n";
                         Result.Content += ans;
-
-                        //checker = 0;
-                        //_timer1Start();
-
                     }
                     else
                     {
+                        AutoClosingMessageBox.Show("Solution Not Found", "Result", 1500);
                         ans = "   TIDAK\n";
                         Result.Content += ans;
                         resetGraph();
@@ -319,7 +344,7 @@ namespace Prototype1
                 now = target;
                 checker = 0;
                 _timer.Start();
-                MessageBox.Show("Hijau " + now);
+                AutoClosingMessageBox.Show("Hijau " + now, "test1", 1000);
                 result.Add(target);
             }
             else if (neighbor == null)
@@ -328,7 +353,7 @@ namespace Prototype1
                 prev = curr;
                 checker = 0;
                 _timer2.Start();
-                MessageBox.Show("Merah " + prev);
+                AutoClosingMessageBox.Show("Merah " + prev, "test2", 1000);
                 result.Remove(curr);
             }
             else
@@ -336,7 +361,7 @@ namespace Prototype1
                 now = curr;
                 checker = 0;
                 _timer.Start();
-                MessageBox.Show("Hijau " + now);
+                AutoClosingMessageBox.Show("Hijau " + now, "test3", 1000);
                 result.Add(curr);
                 int i = 0;
                 while ((i < neighbor.Count) && (!found))
@@ -349,7 +374,7 @@ namespace Prototype1
                     prev = curr;
                     checker = 0;
                     _timer2.Start();
-                    MessageBox.Show("Merah " + prev);
+                    AutoClosingMessageBox.Show("Merah " + prev, "test4", 1000);
                     result.Remove(curr);
                 }
             }
